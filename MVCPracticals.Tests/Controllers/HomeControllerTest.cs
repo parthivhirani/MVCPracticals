460a3a55d7367c377d68a3490383c69b3c666344
MVC Practicals <parthivhirani2903@gmail.com>
2023-06-26 15:44:36 +0530

    Added Practical-09 ASP .NET MVC

---
+++ MVCPracticals.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCPracticals;
+using MVCPracticals.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace MVCPracticals.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Hello World", result.ViewBag.Message);
+        }
+    }
+}
