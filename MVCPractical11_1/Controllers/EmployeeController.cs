687b94c6e581b6638fee90a39c6ee3017a6ede16
MVC Practicals <parthivhirani2903@gmail.com>
2023-07-01 16:53:43 +0530

    Added Practical-11 (Test-1)

---
+++ MVCPractical11_1/Controllers/EmployeeController.cs
@@ -0,0 +1,64 @@
+﻿using MVCPractical11_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractical11_2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        static List<Employee> empList = new List<Employee>()
+        {
+            new Employee(){Id=1, Name="Parthiv", DOB=("29/03/2002").ToString(), Address="Rajkot"},
+            new Employee(){Id=2, Name="Meet", DOB=("06/05/2002").ToString(), Address="Ahmedabad"}
+        };
+        
+        public ActionResult Index()
+        {
+            return View(empList);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(empList[id-1]);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            emp.Id = empList.Count()+1;
+            empList.Add(emp);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(empList[id-1]);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee emp)
+        {
+            var edited = empList[emp.Id-1];
+
+            emp.Id = edited.Id;
+            empList.Add(emp);
+
+            empList.Remove(edited);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            empList.Remove(empList[id-1]);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCPractical11_1/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractical11_1.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name="Date of Birth")]
+        [DisplayFormat(ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public string DOB { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
